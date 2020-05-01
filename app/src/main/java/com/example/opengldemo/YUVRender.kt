9cc09783b3dc2cc05c2d5cee7c16d39c379a1a58
alaiye <alaiye_work@163.com>
2020-05-01 17:15:56 +0800

    gl draw i420

--- app/src/main/java/com/example/opengldemo/YUVRender.kt
+++ app/src/main/java/com/example/opengldemo/YUVRender.kt
@@ -18,7 +18,8 @@ class YUVRender(context: Context) : GLSurfaceView.Renderer {
 
     companion object {
         private const val TAG = "YUVRender"
-
+        // 0 -> i420, 1 -> nv21
+        private const val YUV_TYPE = 0
         private val VERTEXT = floatArrayOf(
             -1f, -1f, 0.0f, 1.0f,
              1f, -1f, 1.0f, 1.0f,
@@ -34,6 +35,8 @@ class YUVRender(context: Context) : GLSurfaceView.Renderer {
     private var positionLoc = -1
     private var texCoordLoc = -1
     private var yTextureLoc = -1
+    private var uTextureLoc = -1
+    private var vTextureLoc = -1
     private var uvTextureLoc = -1
     private var matrixLoc = -1
 
@@ -44,9 +47,16 @@ class YUVRender(context: Context) : GLSurfaceView.Renderer {
     private val imageWidth = 800
     private val imageHeight = 480
     private var yTextureId = -1
+    private var uTextureId = -1
+    private var vTextureId = -1
     private var uvTextureId = -1
+
+    private var typeLoc = -1
+
     private lateinit var imageBytes: ByteArray
     private lateinit var yBuffer: ByteBuffer
+    private lateinit var uBuffer: ByteBuffer
+    private lateinit var vBuffer: ByteBuffer
     private lateinit var uvBuffer: ByteBuffer
 
     override fun onDrawFrame(gl: GL10?) {
@@ -72,9 +82,21 @@ class YUVRender(context: Context) : GLSurfaceView.Renderer {
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, yTextureId)
         GLES20.glUniform1i(yTextureLoc, 0)
 
-        GLES20.glActiveTexture(GLES20.GL_TEXTURE1)
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, uvTextureId)
-        GLES20.glUniform1i(uvTextureLoc, 1)
+        GLES20.glUniform1i(typeLoc, YUV_TYPE)
+
+        if (YUV_TYPE == 1) {
+            GLES20.glActiveTexture(GLES20.GL_TEXTURE1)
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, uvTextureId)
+            GLES20.glUniform1i(uvTextureLoc, 1)
+        } else {
+            GLES20.glActiveTexture(GLES20.GL_TEXTURE1)
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, uTextureId)
+            GLES20.glUniform1i(uTextureLoc, 1)
+
+            GLES20.glActiveTexture(GLES20.GL_TEXTURE2)
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, vTextureId)
+            GLES20.glUniform1i(vTextureLoc, 2)
+        }
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4)
         GLES20.glDisableVertexAttribArray(positionLoc)
@@ -100,8 +122,11 @@ class YUVRender(context: Context) : GLSurfaceView.Renderer {
         positionLoc = GLES20.glGetAttribLocation(program, "aPosition")
         texCoordLoc = GLES20.glGetAttribLocation(program, "aTexCoord")
         yTextureLoc = GLES20.glGetUniformLocation(program, "yTexture")
+        uTextureLoc = GLES20.glGetUniformLocation(program, "uTexture")
+        vTextureLoc = GLES20.glGetUniformLocation(program, "vTexture")
         uvTextureLoc = GLES20.glGetUniformLocation(program, "uvTexture")
         matrixLoc = GLES20.glGetUniformLocation(program, "matrix")
+        typeLoc = GLES20.glGetUniformLocation(program, "type")
 
         vertexBuffer = ByteBuffer
             .allocateDirect(VERTEXT.size * 4)
@@ -109,31 +134,66 @@ class YUVRender(context: Context) : GLSurfaceView.Renderer {
             .asFloatBuffer()
         vertexBuffer?.put(VERTEXT)
 
-        // read image data
-        imageBytes = Util.read("test.yuv", context!!)
-        Log.d(TAG, "onSurfaceCreated: image bytes length = ${imageBytes.size}")
+        if (YUV_TYPE == 1) {
+            // read image data
+            imageBytes = Util.read("test.yuv", context!!)
+            Log.d(TAG, "onSurfaceCreated: image bytes length = ${imageBytes.size}")
 
-        yBuffer = ByteBuffer.allocateDirect(imageWidth * imageHeight)
-            .order(ByteOrder.nativeOrder())
-        yBuffer.put(imageBytes, 0, imageWidth * imageHeight)
-        yBuffer.position(0)
+            yBuffer = ByteBuffer.allocateDirect(imageWidth * imageHeight)
+                .order(ByteOrder.nativeOrder())
+            yBuffer.put(imageBytes, 0, imageWidth * imageHeight)
+            yBuffer.position(0)
 
-        uvBuffer = ByteBuffer.allocateDirect(imageWidth * imageHeight / 2)
-            .order(ByteOrder.nativeOrder())
-        uvBuffer.put(imageBytes, imageWidth * imageHeight, imageWidth * imageHeight / 2)
-        uvBuffer.position(0)
+            uvBuffer = ByteBuffer.allocateDirect(imageWidth * imageHeight / 2)
+                .order(ByteOrder.nativeOrder())
+            uvBuffer.put(imageBytes, imageWidth * imageHeight, imageWidth * imageHeight / 2)
+            uvBuffer.position(0)
 
-        val textureObjectIds = IntArray(2)
-        GLES20.glGenTextures(2, textureObjectIds, 0)
+            val textureObjectIds = IntArray(2)
+            GLES20.glGenTextures(2, textureObjectIds, 0)
 
-        // y texture
-        yTextureId = textureObjectIds[0]
-        textureLuminance(yBuffer, imageWidth, imageHeight, yTextureId)
+            // y texture
+            yTextureId = textureObjectIds[0]
+            textureLuminance(yBuffer, imageWidth, imageHeight, yTextureId)
 
-        // uv texture
-        uvTextureId = textureObjectIds[1]
-        textureLuminanceAlpha(uvBuffer, imageWidth / 2, imageHeight / 2, uvTextureId)
-        Log.d(TAG, "onSurfaceCreated: yTextureId = $yTextureId, uvTextureId = $uvTextureId")
+            // uv texture
+            uvTextureId = textureObjectIds[1]
+            textureLuminanceAlpha(uvBuffer, imageWidth / 2, imageHeight / 2, uvTextureId)
+        } else {
+            // read image data
+            imageBytes = Util.read("yuv420p.yuv", context!!)
+            Log.d(TAG, "onSurfaceCreated: image bytes length = ${imageBytes.size}")
+
+            yBuffer = ByteBuffer.allocateDirect(imageWidth * imageHeight)
+                .order(ByteOrder.nativeOrder())
+            yBuffer.put(imageBytes, 0, imageWidth * imageHeight)
+            yBuffer.position(0)
+
+            uBuffer = ByteBuffer.allocateDirect(imageWidth * imageHeight / 4)
+                .order(ByteOrder.nativeOrder())
+            uBuffer.put(imageBytes, imageWidth * imageHeight, imageWidth * imageHeight / 4)
+            uBuffer.position(0)
+
+            vBuffer = ByteBuffer.allocateDirect(imageWidth * imageHeight / 4)
+                .order(ByteOrder.nativeOrder())
+            vBuffer.put(imageBytes, imageWidth * imageHeight * 5 / 4, imageWidth * imageHeight / 4)
+            vBuffer.position(0)
+
+            val textureObjectIds = IntArray(3)
+            GLES20.glGenTextures(3, textureObjectIds, 0)
+
+            // y texture
+            yTextureId = textureObjectIds[0]
+            textureLuminance(yBuffer, imageWidth, imageHeight, yTextureId)
+
+            // u texture
+            uTextureId = textureObjectIds[1]
+            textureLuminance(uBuffer, imageWidth / 2, imageHeight / 2, uTextureId)
+
+            // v texture
+            vTextureId = textureObjectIds[2]
+            textureLuminance(vBuffer, imageWidth / 2, imageHeight / 2, vTextureId)
+        }
     }
 
     private fun textureLuminance(imageData: ByteBuffer, width: Int, height: Int, textureId: Int) {
--- app/src/main/res/raw/fragment_yuv.glsl
+++ app/src/main/res/raw/fragment_yuv.glsl
@@ -3,7 +3,11 @@ precision mediump float;
 varying vec2 vTexCoord;
 
 uniform sampler2D yTexture;
+uniform sampler2D uTexture;
+uniform sampler2D vTexture;
 uniform sampler2D uvTexture;
+
+uniform int type;
 
 const mat3 yuv2rgb = mat3(
 1, 0, 1.2802,
@@ -17,11 +21,16 @@ void main() {
     //that's why we're pulling it from the R component, we could also use G or B
     y = texture2D(yTexture, vTexCoord).r;
 
-    //We had put the U and V values of each pixel to the A and R,G,B components of the
-    //texture respectively using GL_LUMINANCE_ALPHA. Since U,V bytes are interspread
-    //in the texture
-    u = texture2D(uvTexture, vTexCoord).a;
-    v = texture2D(uvTexture, vTexCoord).r;
+    if (type == 1) {
+        //We had put the U and V values of each pixel to the A and R,G,B components of the
+        //texture respectively using GL_LUMINANCE_ALPHA. Since U,V bytes are interspread
+        //in the texture
+        u = texture2D(uvTexture, vTexCoord).a;
+        v = texture2D(uvTexture, vTexCoord).r;
+    } else {
+        u = texture2D(uTexture, vTexCoord).r;
+        v = texture2D(vTexture, vTexCoord).r;
+    }
 
     y = 1.1643 * (y - 16.0 / 255.0);
     u = u - 128.0 / 255.0;
